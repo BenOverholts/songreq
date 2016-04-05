9353f75dff60546030e2ebc6417450a2b7338831
BenOverholts <commandershadow@gmail.com>
2016-04-05 16:45:39 -0400

    Rewrite server to use API wrapper

--- server/server.js
+++ server/server.js
@@ -1,33 +1,87 @@
 /*
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
+ * This is the api server for Songreq.me
  *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
+ * For more information, see
+ * https://github.com/BenOverholts/songreq
  */
 
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
+var express = require('express');
+var request = require('request');
+var mongoose = require('mongoose');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var SpotifyWebApi = require('spotify-web-api-node');
 
-var client_id = 'c617b042d42f44a69afdf01bd9dfdcf7'; // Your client id
-var client_secret = '724b41ad51c04cf4bea032d3918911fa'; // Your client secret
-var redirect_uri = 'http://partify.me/callback'; // Your redirect uri
+// TODO Replace with env vars and regen
+var devo = (process.env.SRQ_ENV === 'devo');
+var client_id = process.env.SRQ_CLIENT_ID; // Your client id
+var client_secret = process.env.SRQ_CLIENT_SECRET; // Your client secret
+var scopes = ['playlist-modify-public', 'playlist-modify-private']
+var redirect_uri = 'http://localhost:8080/callback'; // Your redirect uri
 var api_root = 'https://api.spotify.com/v1/';
 
-//TODO Use some actual persistant storage
-var partyRequests = {};
-var partyPlids = {};
-var uid;
+mongoose.connect('mongodb://localhost/songreq');
 
-var spotifyApi = new SpotifyWebApi({
-  clientId : client_id,
-  clientSecret : client_secret,
-  redirectUri : redirect_uri
+
+
+/********************************************
+ *
+ * DB Schema Setup
+ *
+ ********************************************/
+var partySchema = new mongoose.Schema({
+  uid: String,
+  plid: String,
+  songs: [{
+    uri: String,
+    name: String,
+    artist: String,
+    artUrl: String
+  }]
 });
+
+// Add a song to a party's requests
+partySchema.methods.addSong = function(uri, name, artist, artUrl, callback) {
+  this.songs.push({
+    uri: uri,
+    name: name,
+    artist: artist,
+    artUrl: artUrl
+  });
+  this.save(function(err, party) {
+    if (callback) callback(err, party);
+  });
+};
+
+// Remove song from a party's requests
+partySchema.methods.removeSong = function(uri, callback) {
+  var index = this.songs.map(function(s) { return s.uri; }).indexOf(uri);
+  this.songs.splice(index, 1);
+  this.save(function(err, party) {
+    if (callback) callback(err, party);
+  });
+};
+
+// Finalize schema model
+var Party = mongoose.model('Party', partySchema);
+
+
+
+/********************************************
+ *
+ * Spotify API Wrapper Setup
+ *
+ * Need a new instance within each route handler so that
+ * user credentials are properly segregated
+ *
+ ********************************************/
+var getSpotifyApi = function() {
+  return new SpotifyWebApi({
+    clientId : client_id,
+    clientSecret : client_secret,
+    redirectUri : redirect_uri
+  });
+};
 
 /**
  * Generates a random string containing numbers and letters
@@ -46,6 +100,11 @@ var generateRandomString = function(length) {
 
 var stateKey = 'spotify_auth_state';
 
+/********************************************
+ *
+ * Express / Route Handlers
+ *
+ ********************************************/
 var app = express();
 
 // Add headers
@@ -68,202 +127,221 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Create a new playlist and set up a party
 app.get('/api/create', function(req, res) {
-    console.log("Handling /create");
+    console.log("[/create] Begin handling");
 
-    // Create new party
+    var spotifyApi = getSpotifyApi();
     var access_token = req.param('access_token');
     var uid = req.param('uid');
-    partyRequests[uid] = [];
     var datetime = new Date();
 
-
-    // Create Playlist
     spotifyApi.setAccessToken(access_token);
 
+    // Create playlist
     spotifyApi.createPlaylist(uid, 'Partify (' + datetime + ')', { 'public': true })
         .then(function(data) {
-            console.log('Success!');
-            console.log(data.body.id);
-            partyPlids[uid] = data.body.id;
+            console.log('[/create] Successfully created playlist ' + data.body.id);
+            
+            // Create party
+            var party = Party({
+              uid: uid,
+              plid: data.body.id,
+              songs: []
+            });
+
+            // Persist party
+            party.save(function(err, party) {
+              if (err) {
+                console.log('[/create] Error saving party' + party.uid);
+                res.status(500).send('Error creating party');
+              }
+
+              console.log('[/create] Instantiated party ' + party.uid);
+              console.log('[/create] Complete')
+              res.sendStatus(200);
+            });
+
         }, function(err) {
-            console.log("Fuck... ", err);
+            console.log("[/create] Error interacting with Spotify...", err);
+            res.status(500).send('Error interacting with Spotify');
         });
-
-    res.send( {'success': true} );
 });
 
+// Get the list of song requests for a party
 app.get('/api/requests', function(req, res) {
-    console.log('Handling /requests');
-    // Show all pending requests
+    console.log('[/requests] Begin handling');
     var uid = req.param('uid');
-    if (!partyRequests[uid]) {
-        partyRequests[uid] = [];
-    }
-    res.send( partyRequests[uid] );
+
+    // Fetch party from storage
+    Party.find({ uid: uid }, function(err, party) {
+      if (err) {
+        console.log("[/requests] Error finding party " + uid);
+        res.status(400).send("Error finding party " + uid);
+      }
+
+      // Respond with the song requests
+      console.log('[/requests] Complete');
+      res.send( party.songs );
+    });
 });
 
-/*{
-    "122879": [
-    { "uri": "asdf ", "name": "lakjsdfls", "artist": "alkbj"},
-    { "" },
-    ],
-    ...
-
-] */
-// /api/dismiss?party="pid"&uri="spotify:1231adfasdf24332"
+// Remove a song request from the request queue for a party
 app.get('/api/dismiss', function(req, res) {
-    console.log('Handling /dismiss');
+    console.log('[/dismiss] Begin handling');
     var uri = req.param('uri');
-    var uid = req.param('uid');
-    var party = partyRequests[uid];
-    var remove = -1;
-    for (var i in party) {
-        if (party[i]['uri'] == uri) {
-            remove = i;
+
+    // Fetch party from storage
+    Party.find({ uid: req.param('uid') }, function(err, party) {
+      if (err) {
+        console.log("[/dismiss] Error finding party " + req.param('uid'));
+        res.status(400).send("Error finding party " + req.param('uid'));
+      }
+
+      // Remove song
+      party.removeSong(uri, function(err, party) {
+        if (err) {
+          console.log("[/dismiss] Error removing song " + uri +
+            " from party " + party.uid);
+          res.status(500).send("Error removing song " + uri);
         }
-    }
-    if (remove >= 0) {
-        partyRequests['uid'] = party.splice(remove, 1);
-    }
-    res.send({'success': true});
+
+        console.log("[/dismiss] Successfully removed song " + uri +
+          " from party " + party.uid);
+        console.log('[/dismiss] Complete');
+        res.sendStatus(200);
+      });
+    });
 });
 
+// Add a requested song to the party playlist
 app.get('/api/approve', function(req, res) {
-    // DJ Adds song to playlist
-    var uri = req.param('uri');
-    var uid = req.param('uid');
-    var access_token = req.param("access_token");
+  console.log('[/approve] Begin handling');
 
-    // Add to playlist
+  // DJ Adds song to playlist
+  var spotifyApi = getSpotifyApi();
+  var uri = req.param('uri');
+  var uid = req.param('uid');
+  var access_token = req.param('access_token');
+
+  // Fetch party from storage
+  Party.find({ uid: req.param('uid') }, function(err, party) {
+    if (err) {
+      console.log('[/approve] Error finding party ' + uid);
+      res.status(400).send('Error finding party ' + uid);
+    }
+
     spotifyApi.setAccessToken(access_token);
 
-    spotifyApi.addTracksToPlaylist(uid, partyPlids[uid], [uri])
+    // Add to playlist
+    spotifyApi.addTracksToPlaylist(uid, party.plid, [uri])
       .then(function(data) {
-        console.log('Added tracks to playlist!');
+        console.log('[/approve] Succesfully added song' + uri + 
+          ' to playlist ' + party.plid);
+        party.removeSong(uri, function(err, party) {
+          if (err) {
+            console.log('[/approve] Error removing song ' + uri +
+              ' from party ' + party.uid);
+          }
+
+          console.log('[/approve] Successfully removed song ' + uri +
+            ' from party ' + party.uid);
+          console.log('[/approve] Complete');
+          res.sendStatus(200);
+        });
       }, function(err) {
-        console.log('Something went wrong!', err);
+        console.log('[/approve] Error interacting with Spotify...', err);
+        res.status(500).send('Error interacting with Spotify');
       });
 
-    var party = partyRequests[uid];
-    var remove = -1;
-    for (var i in party) {
-        if (party[i]['uri'] == uri) {
-            remove = i;
-        }
-    }
-    if (remove >= 0) {
-        partyRequests['uid'] = party.splice(remove, 1);
-    }
-    res.send({'success': true});
+  });
 });
 
+// Add a song request to a party
 app.get('/api/request', function(req, res) {
-    console.log('Handling /request');
+    console.log('[/request] Begin handling');
+
     // Add user submitted request to requests list
     var uri = req.param('uri');
     var uid = req.param('uid');
-    var song_name = req.param('song_name');
+    var name = req.param('song_name');
     var artist = req.param('artist');
+    var artUrl = 'http://placehold.it/350x350'; // TODO Pass url from Angular
 
-    partyRequests[uid].push({
-        "uri": uri,
-        "name": song_name,
-        "artist": artist
+    Party.find({ uid: req.param('uid') }, function(err, party) {
+      if (err) {
+        console.log('[/request] Error finding party ' + uid);
+        res.status(400).send('Error finding party ' + uid);
+      }
+
+      party.addSong(uri, name, artist, artUrl, function(err, party) {
+        if (err) {
+          console.log("[/request] Error adding song " + uri +
+            " to party " + party.uid);
+          res.status(500).send("Error adding song " + uri);
+        }
+
+        console.log("[/request] Successfully removed song " + uri +
+          " from party " + party.uid);
+        console.log('[/request] Complete');
+        res.sendStatus(200);
+      });
     });
-
-    //console.log(partyRequests);
-    //console.log(partyRequests[uid]);
-    res.send( {'success': true} );
 });
 
+// Initiate Spotify login flow
 app.get('/api/login', function(req, res) {
+  console.log('[/login] Redirected user to Spotify Authorization');
 
+  var spotifyApi = getSpotifyApi();
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  // your application requests authorization
-  var scope = 'playlist-modify-public playlist-modify-private';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+  // Direct user to Spotify authorization
+  res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
 });
 
+// Retrieve access token with auth code granted by user
 app.get('/callback', function(req, res) {
+  console.log('[/callback] Begin handling');
 
-  // your application requests refresh and access tokens
-  // after checking the state parameter
+  var spotifyApi = getSpotifyApi();
+  var code = req.param('code') || null;
 
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
+  if (code === null) {
+    console.log('[/callback] Error reading authorization code');
+    res.status(400).send('Error reading authorization code');
   } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
+    spotifyApi.authorizationCodeGrant(code)
+      .then(function(data) {
 
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
+        console.log('[/callback] Successfully retrieved access token');
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
 
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+        // Fetch the user's spotify UID
+        spotifyApi.getMe()
+          .then(function(data) {
+            console.log('[/callback] Successfully retrieved UID ' + data.body.id);
 
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
+            // Set cookies and redirect them back to the client
+            // TODO {secure: true} once HTTPS
+            res.cookie('access_token', spotifyApi.getAccessToken());
+            res.cookie('refresh_token', spotifyApi.getRefreshToken());
+            res.cookie('uid', data.body.id);
 
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                res.cookie('access_token', access_token);
-                res.cookie('refresh_token', refresh_token);
-                res.cookie('uid', body.id);
-                res.redirect('/login?' +
-                    querystring.stringify({
-                        access_token: access_token,
-                        refresh_token: refresh_token,
-                        uid: body.id,
-                        session: (partyRequests[body.id] != undefined)
-                }));
-            } else {
-                console.log(error);
-            }
-        });
-
-
-            // we can also pass the token to the browser to make requests from there
-            //res.cookie('access_token', access_token);
-            //res.cookie('refresh_token', refresh_token);
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
+            console.log('[/callback] Complete');
+            console.log('Devo: ' + devo);
+            console.log(((devo)?'http://localhost:3000':'') + '/login');
+            res.redirect(((devo)?'http://localhost:3000':'') + '/login');
+          }, function(err) {
+            console.log('[/callback] Error retrieving UID', err);
+            res.status(500).send('Error retrieving UID');
+          });
+      }, function(err) {
+        console.log('[/callback] Error retrieving authorization token');
+        res.status(500).send('Error retrieving authorization token');
+      });
   }
 });
 
