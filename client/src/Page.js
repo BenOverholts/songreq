dd84484cf3ac42ce72654aca807b6815e5de79ad
overholts <ben.overholts@gmail.com>
2018-03-07 14:24:51 -0800

    (Refactor) Create reusable HOC for Page composition

---
+++ client/src/Page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+import Header from './Header';
+
+export default function asPage(PageComponent) {
+  return class extends Component {
+    render() {
+      return (
+        <div className="container body-container">
+          <Header />
+          <PageComponent />
+        </div>
+      );
+    }
+  }
+}
--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
 import './Welcome.css';
-import Header from './Header';
 import Button from './Button';
+import asPage from './Page';
 
 class Welcome extends Component {
   render() {
     return (
-      <div className="container body-container">
-        <Header />
-        <div className="Welcome">
-          <div className="row">
-            <div className="col-12">
-              <h3>Welcome</h3>
-              <p className="welcome-text">
-                Play the music people want to hear, while staying in control of the turntables.
-                If you're hosting a party, just log in to Spotify to let people start suggesting songs.
-              </p>
-            </div>
+      <div className="Welcome">
+        <div className="row">
+          <div className="col-12">
+            <h3>Welcome</h3>
+            <p className="welcome-text">
+              Play the music people want to hear, while staying in control of the turntables.
+              If you're hosting a party, just log in to Spotify to let people start suggesting songs.
+            </p>
           </div>
-          <div className="row">
-            <div className="col-md-6 text-center top-margin">
-              <Button kind="primary" content="Hosting? Log In" className="hosting-button" />
-            </div>
-            <div className="col-md-6 text-center top-margin">
-              <Button kind="secondary" content="Join a Party" className="join-button" />
-            </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 text-center top-margin">
+            <Button kind="primary" content="Hosting? Log In" className="hosting-button" />
+          </div>
+          <div className="col-md-6 text-center top-margin">
+            <Button kind="secondary" content="Join a Party" className="join-button" />
           </div>
         </div>
       </div>
@@ -32,4 +29,4 @@ class Welcome extends Component {
   }
 }
 
-export default Welcome;
+export default asPage(Welcome);
